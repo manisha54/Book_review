a9f275ef4d35de074c342b56885b6a77f00f26b3
manisha chaudhary <manishachaudhary2057@gmail.com>
2023-06-20 11:52:59 +0545

    api testing

--- index.js
+++ index.js
@@ -1,72 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
 
-const books_routes = require('./routes/book-routes')
-const user_routes  =require('./routes/user_routes')
-const {verifyUser} = require('./middlewares/auth')
-const upload = require('./middlewares/upload')
-
-
-
+const app = require('./app')
 const port = process.env.PORT
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/Books-Review')
-  .then(()=>{
-    console.log('connected to the mongodb database server')
-  })
-  .catch((err) => console.log(err))
-
-
-
-
-const app = express()  //initiate  express by giving name app
-
-app.use(express.json())   //request pass through this
-
-app.use(express.static('public'))  //for see image by image path   ---upload/path of image
-
-
-
-
-
-app.get('/', (req,res) =>{
-  //  console.log(req)
-    res.send("aratiiii chaprii nibiii")
-
-})
-
-
-app.use('/users', user_routes )
-//app.use(verifyUser)            //call middleware
-app.use('/books', books_routes )
-
-app.post('/upload', upload.single('photo'), (req, res, next)=>{
-  res.json(req.file)
-
-})
-
-
-//error handling middleware
-app.use((err,req, res,next)=>{
-  console.error(err)
-  if (err.name === 'ValidatioError') res.status(400)
-  else if(err.name === 'CastError')res.status(400)
-  console.log(err.message)
-  res.json({error:err.message})
-})
-
-
-
-//unknown path
-
-app.use((req, res) =>{
-  res.status(404).json({error: "path not found"})
-})
-
-
-
 app.listen(port,()=>{
-    console.log(`server is running at port ${port}`)
+  console.log(`server is running at port ${port}`)
 })
---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const app = require('../app')
+
+//send request
+const supertest = require('supertest')  
+
+const api = supertest(app)
+
+test('of root path', async() =>{
+    const res = await api.get('/')
+    expect(res.statusCode).toBe(200)
+    expect(res.text).toMatch(/aratiiii chaprii nibiii/)
+})
